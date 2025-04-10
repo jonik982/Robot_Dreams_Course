cdcc6c2b6fc0dd565cc8e1f62e26f8285f3490b1
jonik982 <42321656+jonik982@users.noreply.github.com>
2025-04-10 13:36:45 +0300

    Create sales_api.py

---
+++ job1/bll/sales_api.py
@@ -0,0 +1,53 @@
+import os
+
+from Lecture_2.ht_template.job1.dal import local_disk, sales_api
+
+
+
+
+def save_sales_to_local_disk(date: str, raw_dir: str) -> None:
+    # TODO: implement me
+    # 1. get data from the API
+    # 2. save data to disk
+
+
+
+    jsondata = sales_api.get_sales(date)
+
+
+
+    file_name = "sales_" + date + ".json"
+
+
+
+    raw_dir_local = os.path.join(raw_dir, file_name)
+
+    folderpath = os.path.join(raw_dir)
+
+
+
+
+
+
+
+    if os.path.exists(raw_dir_local):
+        os.remove(raw_dir_local)
+
+
+
+    if os.path.exists(folderpath):
+            os.rmdir(folderpath)
+
+
+    if not os.path.exists(folderpath):
+        os.mkdir(folderpath)
+
+
+
+
+    local_disk.save_to_disk(jsondata, raw_dir_local)
+
+
+    pass
+
+
